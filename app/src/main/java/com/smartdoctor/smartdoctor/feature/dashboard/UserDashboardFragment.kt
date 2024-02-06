063f3d6ab8e591cd0a64fbbc704247a607813943
mostafa <mostafaabdalla779933@gmail.com>
2024-02-06 22:51:13 +0200

    commit

--- app/src/main/java/com/smartdoctor/smartdoctor/feature/dashboard/UserDashboardFragment.kt
+++ app/src/main/java/com/smartdoctor/smartdoctor/feature/dashboard/UserDashboardFragment.kt
@@ -1,12 +1,24 @@
 package com.smartdoctor.smartdoctor.feature.dashboard
 
 import com.donationinstitutions.donationinstitutions.common.base.BaseFragment
+import com.donationinstitutions.donationinstitutions.common.firebase.FirebaseHelp
 import com.smartdoctor.smartdoctor.databinding.FragmentUserDashboardBinding
 
 class UserDashboardFragment : BaseFragment<FragmentUserDashboardBinding>() {
     override fun initBinding() = FragmentUserDashboardBinding.inflate(layoutInflater)
 
     override fun onFragmentCreated() {
+        handleCustomBack {
+            requireActivity().finish()
+        }
+
+        binding.apply {
+            ivProfile.setOnClickListener {
+                FirebaseHelp.logout()
+                requireActivity().finish()
+                requireActivity().startActivity(requireActivity().intent)
+            }
+        }
     }
 
     override fun reload() {
