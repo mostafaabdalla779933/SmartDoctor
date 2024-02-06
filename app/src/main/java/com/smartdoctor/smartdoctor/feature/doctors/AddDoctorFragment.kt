d3cb208ae2af49760a92457e4aa752dca80d7ca6
Mina Ezzat <mf502308@gmail.com>
2024-02-06 23:27:42 +0200

    ui

---
+++ app/src/main/java/com/smartdoctor/smartdoctor/feature/doctors/AddDoctorFragment.kt
@@ -0,0 +1,14 @@
+package com.smartdoctor.smartdoctor.feature.doctors
+
+import com.donationinstitutions.donationinstitutions.common.base.BaseFragment
+import com.smartdoctor.smartdoctor.databinding.FragmentAddDoctorBinding
+
+class AddDoctorFragment : BaseFragment<FragmentAddDoctorBinding>() {
+    override fun initBinding() = FragmentAddDoctorBinding.inflate(layoutInflater)
+    override fun onFragmentCreated() {
+
+    }
+
+    override fun reload() {}
+
+}
--- app/src/main/java/com/smartdoctor/smartdoctor/feature/doctors/DoctorsFragment.kt
+++ app/src/main/java/com/smartdoctor/smartdoctor/feature/doctors/DoctorsFragment.kt
@@ -1,17 +1,10 @@
 package com.smartdoctor.smartdoctor.feature.doctors
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.donationinstitutions.donationinstitutions.common.base.BaseFragment
-import com.smartdoctor.smartdoctor.R
 import com.smartdoctor.smartdoctor.databinding.FragmentDoctorsBinding
 
-
 class DoctorsFragment : BaseFragment<FragmentDoctorsBinding>() {
-    override fun initBinding()=FragmentDoctorsBinding.inflate(layoutInflater)
+    override fun initBinding() = FragmentDoctorsBinding.inflate(layoutInflater)
     override fun onFragmentCreated() {
 
     }
