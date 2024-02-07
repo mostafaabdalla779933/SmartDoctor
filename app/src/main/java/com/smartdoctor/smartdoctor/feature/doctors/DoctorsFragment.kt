978df14236d61306cd339d99823c34d48c344f4d
mostafa <mostafaabdalla779933@gmail.com>
2024-02-07 19:36:41 +0200

    commit

--- app/src/main/java/com/smartdoctor/smartdoctor/feature/doctors/DoctorsFragment.kt
+++ app/src/main/java/com/smartdoctor/smartdoctor/feature/doctors/DoctorsFragment.kt
@@ -1,11 +1,22 @@
 package com.smartdoctor.smartdoctor.feature.doctors
 
+import androidx.navigation.fragment.findNavController
 import com.donationinstitutions.donationinstitutions.common.base.BaseFragment
 import com.smartdoctor.smartdoctor.databinding.FragmentDoctorsBinding
 
 class DoctorsFragment : BaseFragment<FragmentDoctorsBinding>() {
     override fun initBinding() = FragmentDoctorsBinding.inflate(layoutInflater)
     override fun onFragmentCreated() {
+
+        binding.apply {
+            ivBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            ivAdd.setOnClickListener {
+                findNavController().navigate(DoctorsFragmentDirections.actionDoctorsFragmentToAddDoctorFragment())
+            }
+        }
 
     }
 
