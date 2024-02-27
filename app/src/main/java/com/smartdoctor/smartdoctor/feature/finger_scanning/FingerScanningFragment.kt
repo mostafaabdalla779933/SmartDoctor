19f270e4d2860cb1c44445c6ab943a9dc45abe13
Mina Ezzat <mf502308@gmail.com>
2024-02-27 23:33:00 +0200

    added finger scan

--- app/src/main/java/com/smartdoctor/smartdoctor/feature/finger_scanning/FingerScanningFragment.kt
+++ app/src/main/java/com/smartdoctor/smartdoctor/feature/finger_scanning/FingerScanningFragment.kt
@@ -1,12 +1,32 @@
 package com.smartdoctor.smartdoctor.feature.finger_scanning
 
+import android.os.Handler
+import android.os.Looper
 import com.smartdoctor.smartdoctor.common.base.BaseFragment
 import com.smartdoctor.smartdoctor.databinding.FragmentFingerScanningBinding
+import java.util.Random
 
-class FingerScanningFragment  : BaseFragment<FragmentFingerScanningBinding>() {
+class FingerScanningFragment : BaseFragment<FragmentFingerScanningBinding>() {
     override fun initBinding() = FragmentFingerScanningBinding.inflate(layoutInflater)
 
     override fun onFragmentCreated() {
+        binding.apply {
+            ivFinger.setOnLongClickListener {
+                showLoading()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    hideLoading()
+                    tvHeartPulse.text = generateRandomValue(60, 100).toString()
+                    tvStress.text = "${generateRandomValue(2, 7)}%"
+                    tvOxygenRatio.text = "${generateRandomValue(95, 100)}%"
+                }, 1000)
+            }
+        }
+
+    }
+
+    private fun generateRandomValue(min: Int, max: Int): Int {
+        val random = Random()
+        return random.nextInt((max - min) + 1) + min
     }
 
     override fun reload() {
